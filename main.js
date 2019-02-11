eb7957979bb7e4e410754b01840b1185f4239bfb
jessachalla <jessa.challa@gvmc.org>
2019-02-11 18:07:18 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,28 @@
+//First part of the exersice
+// let actualTemp = 90;
+// let desiredTemp = 80;
+
+// if (actualTemp > desiredTemp) {
+// console.log("Run A/C");
+// }
+// else if (actualTemp < desiredTemp) {
+// console.log("Run heat");
+// }
+// else {
+// console.log("Standby"); 
+// }
+
+//Extended Challenge - Switch Statement
+let tempCelsius = 20; //This is the current temp in CELSIUS
+let targetUnit = "K"; //This is the unit I want to change the temp to
+
+switch(targetUnit) {
+    case "F":
+        console.log ((tempCelsius * 9/5 + 32)+ "F");
+        break;
+    case "K":
+        console.log ((tempCelsius + 273.5)+ "K");
+        break;
+    case "C":
+        console.log (tempCelsius + "C");
+}
